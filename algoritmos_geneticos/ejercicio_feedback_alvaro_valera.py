5d144a978b00107561da614e9d405f73d4d5209e
Alvaro Valera <alvarovaleralopez@gmail.com>
2023-05-19 00:10:26 +0200

    Se añaden comentarios al código porpuesto por GPT 4.

--- algoritmos_geneticos/ejercicio_feedback_alvaro_valera.py
+++ algoritmos_geneticos/ejercicio_feedback_alvaro_valera.py
@@ -2,31 +2,32 @@ import random
 import numpy as np
 
 # Parámetros del algoritmo
-ELEMENTOS_POBLACION = 100
-MAX_ITERACIONES = 1000
-CODIGO = np.array([30, 1, 50, 77, 85, 99, 2, 8, 40, 22])  # Ejemplo de código a buscar
-LON_CROMOSOMA = len(CODIGO)
-PROB_MUTACION = 0.1
+ELEMENTOS_POBLACION = 100  # Tamaño de la población
+MAX_ITERACIONES = 1000  # Número máximo de iteraciones
+CODIGO = np.array([30, 1, 50, 77, 85, 99, 2, 8, 40, 22])  # Código a encontrar
+LON_CROMOSOMA = len(CODIGO)  # Longitud del cromosoma (igual a la longitud del código)
+PROB_MUTACION = 0.1  # Probabilidad de mutación
 
-# Creación de la población inicial
+# Creación de la población inicial con cromosomas generados aleatoriamente
 def primeraGeneracion():
     return [np.random.randint(1, 100, LON_CROMOSOMA) for _ in range(ELEMENTOS_POBLACION)]
 
-# Cálculo del fitness
+# Cálculo del fitness de un cromosoma, se cuenta el número de dígitos que difieren del código objetivo
 def evaluarCromosoma(cromosoma):
     return np.sum(cromosoma != CODIGO)
 
-# Ordenar la población
+# Ordenar la población por su fitness (los individuos con menor fitness, es decir, más parecidos al código, primero)
 def ordenarPoblacion(poblacion):
     poblacion.sort(key=evaluarCromosoma)
     return poblacion
 
-# Creación de la siguiente generación
+# Creación de la siguiente generación a partir de la generación actual
 def siguienteGeneracion(poblacion):
-    nueva_generacion = []
-    elite = poblacion[:ELEMENTOS_POBLACION // 10]
-    nueva_generacion.extend(elite)
+    nueva_generacion = []  # Lista para la nueva generación
+    elite = poblacion[:ELEMENTOS_POBLACION // 10]  # Selección del 10% de los mejores individuos
+    nueva_generacion.extend(elite)  # La élite pasa directamente a la siguiente generación
     
+    # Completamos la población con descendencia de individuos elegidos de la élite y la población
     while len(nueva_generacion) < ELEMENTOS_POBLACION:
         padre = random.choice(elite)
         madre = random.choice(poblacion)
@@ -35,13 +36,13 @@ def siguienteGeneracion(poblacion):
 
     return nueva_generacion
 
-# Cruce de dos individuos
+# Cruce de dos cromosomas, se toma una parte del padre y una del madre
 def cruzar(padre, madre):
     punto_cruce = random.randint(0, LON_CROMOSOMA)
     hijo = np.concatenate((padre[:punto_cruce], madre[punto_cruce:]))
     return hijo
 
-# Mutación
+# Mutación de la población, con una probabilidad definida, un cromosoma puede mutar (cambiar un dígito aleatoriamente)
 def mutacion(poblacion):
     for i in range(len(poblacion)):
         if random.random() < PROB_MUTACION:
@@ -49,7 +50,7 @@ def mutacion(poblacion):
             poblacion[i][punto_mutacion] = random.randint(1, 100)
     return poblacion
 
-# Algoritmo principal
+# Algoritmo principal que controla el flujo del proceso evolutivo
 def algoritmo_genetico():
     poblacion = primeraGeneracion()
     poblacion = ordenarPoblacion(poblacion)
@@ -59,13 +60,16 @@ def algoritmo_genetico():
         poblacion = mutacion(poblacion)
         poblacion = ordenarPoblacion(poblacion)
 
+        # Si el mejor individuo es igual al código, se ha encontrado la solución
         if evaluarCromosoma(poblacion[0]) == 0:
             print("Código encontrado!")
             print(poblacion[0])
             return poblacion[0]
 
+    # Si se llega al número máximo de iteraciones y no se ha encontrado la solución, se informa y se termina
     print("Código no encontrado en las iteraciones máximas.")
     return None
+
 
 if __name__ == '__main__':
     # Ejecutar el algoritmo genético
