6366361f4b582e6b2cd50ac6ea94cf2640cfe641
Alvaro Valera <alvarovaleralopez@gmail.com>
2023-05-19 00:06:43 +0200

    Código propuesto por GPT 4.

--- algoritmos_geneticos/ejercicio_feedback_alvaro_valera.py
+++ algoritmos_geneticos/ejercicio_feedback_alvaro_valera.py
@@ -1,109 +1,72 @@
 import random
+import numpy as np
 
-# Parámetros del algoritmo genético
-TAM_POBLACION = 100  # Tamaño de la población
-LONGITUD_CROMOSOMA = 10  # Longitud del cromosoma
-NUM_GENERACIONES = 100  # Número de generaciones
-ELITE_PORCENTAJE = 0.1  # Porcentaje de élite
-PROB_MUTACION = 0.1  # Probabilidad de mutación
-MAX_ITERACIONES = 1000  # Número máximo de iteraciones
+# Parámetros del algoritmo
+ELEMENTOS_POBLACION = 100
+MAX_ITERACIONES = 1000
+CODIGO = np.array([30, 1, 50, 77, 85, 99, 2, 8, 40, 22])  # Ejemplo de código a buscar
+LON_CROMOSOMA = len(CODIGO)
+PROB_MUTACION = 0.1
 
-# Código objetivo
-codigo_objetivo = [5, 17, 92, 43, 78, 32, 66, 10, 55, 25]
+# Creación de la población inicial
+def primeraGeneracion():
+    return [np.random.randint(1, 100, LON_CROMOSOMA) for _ in range(ELEMENTOS_POBLACION)]
 
-class Cromosoma:
-    def __init__(self):
-        self.genes = [random.randint(1, 100) for _ in range(LONGITUD_CROMOSOMA)]
-        self.fitness = 0
+# Cálculo del fitness
+def evaluarCromosoma(cromosoma):
+    return np.sum(cromosoma != CODIGO)
 
-def calcular_fitness(cromosoma):
-    return sum(1 for i, j in zip(cromosoma.genes, codigo_objetivo) if i != j)
+# Ordenar la población
+def ordenarPoblacion(poblacion):
+    poblacion.sort(key=evaluarCromosoma)
+    return poblacion
 
-def primera_generacion():
-    return [Cromosoma() for _ in range(TAM_POBLACION)]
+# Creación de la siguiente generación
+def siguienteGeneracion(poblacion):
+    nueva_generacion = []
+    elite = poblacion[:ELEMENTOS_POBLACION // 10]
+    nueva_generacion.extend(elite)
+    
+    while len(nueva_generacion) < ELEMENTOS_POBLACION:
+        padre = random.choice(elite)
+        madre = random.choice(poblacion)
+        hijo = cruzar(padre, madre)
+        nueva_generacion.append(hijo)
 
-def ordenar_poblacion(poblacion):
-    poblacion.sort(key=lambda cromosoma: cromosoma.fitness)
+    return nueva_generacion
 
-def seleccion(poblacion):
-    elite_size = int(TAM_POBLACION * ELITE_PORCENTAJE)
-    elite = poblacion[:elite_size]
-    seleccionados = elite.copy()
-    for _ in range(TAM_POBLACION - elite_size):
-        padre1 = random.choice(elite)
-        padre2 = random.choice(poblacion)
-        seleccionados.append(padre2 if padre1.fitness < padre2.fitness else padre1)
-    return seleccionados
+# Cruce de dos individuos
+def cruzar(padre, madre):
+    punto_cruce = random.randint(0, LON_CROMOSOMA)
+    hijo = np.concatenate((padre[:punto_cruce], madre[punto_cruce:]))
+    return hijo
 
-def cruzamiento(padres):
-    hijos = []
-    for i in range(0, TAM_POBLACION, 2):
-        punto_corte = random.randint(1, LONGITUD_CROMOSOMA - 1)
-        hijo1 = Cromosoma()
-        hijo2 = Cromosoma()
-        hijo1.genes = padres[i].genes[:punto_corte] + padres[i + 1].genes[punto_corte:]
-        hijo2.genes = padres[i + 1].genes[:punto_corte] + padres[i].genes[punto_corte:]
-        hijos.extend([hijo1, hijo2])
-    return hijos
-
+# Mutación
 def mutacion(poblacion):
-    for cromosoma in poblacion:
-        for i in range(LONGITUD_CROMOSOMA):
-            if random.random() < PROB_MUTACION:
-                cromosoma.genes[i] = random.randint(1, 100)
+    for i in range(len(poblacion)):
+        if random.random() < PROB_MUTACION:
+            punto_mutacion = random.randint(0, LON_CROMOSOMA - 1)
+            poblacion[i][punto_mutacion] = random.randint(1, 100)
+    return poblacion
 
+# Algoritmo principal
 def algoritmo_genetico():
-    poblacion = primera_generacion()
-    mejor_fitness = float('inf')
-    iteracion = 0
+    poblacion = primeraGeneracion()
+    poblacion = ordenarPoblacion(poblacion)
 
-    while mejor_fitness > 0 and iteracion < MAX_ITERACIONES:
-        for cromosoma in poblacion:
-            cromosoma.fitness = calcular_fitness(cromosoma)
+    for _ in range(MAX_ITERACIONES):
+        poblacion = siguienteGeneracion(poblacion)
+        poblacion = mutacion(poblacion)
+        poblacion = ordenarPoblacion(poblacion)
 
-        ordenar_poblacion(poblacion)
-        mejor_cromosoma = poblacion[0]
-        mejor_fitness = mejor_cromosoma.fitness
+        if evaluarCromosoma(poblacion[0]) == 0:
+            print("Código encontrado!")
+            print(poblacion[0])
+            return poblacion[0]
 
-        print(f"Iteración {iteracion + 1}: Fitness={mejor_fitness}, Cromosoma={mejor_cromosoma.genes}")
-
-        seleccionados = seleccion(poblacion)
-        hijos = cruzamiento(seleccionados)
-        mutacion(hijos)
-        ordenar_poblacion(hijos)
-        poblacion = hijos[:TAM_POBLACION]
-        iteracion += 1
-
-    if mejor_fitness == 0:
-        print("¡Se ha encontrado la solución!")
-    else:
-        print("Se ha alcanzado el número máximo de iteraciones sin encontrar la solución.")
-
+    print("Código no encontrado en las iteraciones máximas.")
+    return None
 
 if __name__ == '__main__':
     # Ejecutar el algoritmo genético
-    poblacion = primera_generacion()
-    mejor_fitness = float('inf')
-    iteracion = 0
-
-    while mejor_fitness > 0 and iteracion < MAX_ITERACIONES:
-        for cromosoma in poblacion:
-            cromosoma.fitness = calcular_fitness(cromosoma)
-
-        ordenar_poblacion(poblacion)
-        mejor_cromosoma = poblacion[0]
-        mejor_fitness = mejor_cromosoma.fitness
-
-        print(f"Iteración {iteracion + 1}: Fitness={mejor_fitness}, Cromosoma={mejor_cromosoma.genes}")
-
-        seleccionados = seleccion(poblacion)
-        hijos = cruzamiento(seleccionados)
-        mutacion(hijos)
-        ordenar_poblacion(hijos)
-        poblacion = hijos[:TAM_POBLACION]
-        iteracion += 1
-
-    if mejor_fitness == 0:
-        print("¡Se ha encontrado la solución!")
-    else:
-        print("Se ha alcanzado el número máximo de iteraciones sin encontrar la solución.")
+    algoritmo_genetico()
